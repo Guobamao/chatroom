825f3eb2f0124bc7a94cde8a5c1b9c8c4dde5642
Guobamao <1972212424@qq.com>
2024-10-23 00:24:43 +0800

    feat(api): 添加用户注册接口- 新增 AuthorizeController 类，提供用户注册功能 - 注册接口路径为 /api/auth/register- 使用 AccountService 的 registerAccount 方法进行用户注册 - 返回注册结果，成功则返回成功信息，失败则返回错误信息

---
+++ src/main/java/com/scauzj/chatroom/controller/AuthorizeController.java
@@ -0,0 +1,23 @@
+package com.scauzj.chatroom.controller;
+
+import com.scauzj.chatroom.domain.Account;
+import com.scauzj.chatroom.domain.AjaxResult;
+import com.scauzj.chatroom.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthorizeController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody Account account) {
+        return accountService.registerAccount(account) ? AjaxResult.success() : AjaxResult.error("注册失败");
+    }
+}
